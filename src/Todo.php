c00e236ea607e3a6b91627f923dd3ea45abc5099
Vedat Bozkurt <info@wedat.org>
2020-12-07 23:17:58 +0300

    new branch

--- src/Todo.php
+++ src/Todo.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2020-12-07 23:17:39
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2020-12-07 23:17:39
+ */
 
 namespace Wedat\Todo;
 
@@ -6,6 +13,6 @@ class Todo
 {
     public static function index()
     {
-        echo 'dans';
+        return '';
     }
 }
